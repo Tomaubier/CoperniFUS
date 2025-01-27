06cc4d0098da52823b53c8f9a9f8f597d45f2495
Tom Aubier <tom.aubier@inserm.fr>
2025-01-27 18:44:10 +0100

    test_brain_atlas

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -7,6 +7,6 @@ def viewer_window(qtbot):
     window = Window(app=None, running_test=True)
     return window
 
-def test_window_title(viewer_window):
-    """Test the window title."""
-    assert viewer_window.windowTitle() == "CoperniFUS"
+def test_brain_atlas(viewer_window):
+    """Test tha the example atlas has been loaded."""
+    assert viewer_window.get_module_object_from_name('BrainAtlas').bg_atlas.atlas_name == 'example_mouse_100um'
