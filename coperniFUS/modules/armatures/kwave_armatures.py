3deb80b205bdb50b2751f9cca455a4131bf07d2e
Tom Aubier <tom.aubier@inserm.fr>
2025-02-06 17:40:00 +0100

    trimesh interface import bug fix

--- coperniFUS/modules/armatures/kwave_armatures.py
+++ coperniFUS/modules/armatures/kwave_armatures.py
@@ -2,6 +2,7 @@ from coperniFUS import *
 from coperniFUS.modules.armatures.mesh_armatures import STLMeshBooleanArmature
 from coperniFUS.modules.armatures.base_armature import Armature
 from coperniFUS.modules.interfaces.kwave_interfaces import *
+from coperniFUS.modules.interfaces.trimesh_interfaces import *
 
 
 class KwaveAShomogeneousSimulationArmature(Armature):
