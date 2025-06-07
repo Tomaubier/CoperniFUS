69ecadd7e640950f0371dbe81168742216a1cade
Tom Aubier <tom.aubier@inserm.fr>
2025-06-07 16:35:59 +0200

    Create new config when running tests

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -15,6 +15,7 @@ reference_test_assets_dir_path = pathlib.Path(coperniFUS.__file__).parent.parent
 def viewer_window(qapp):
     """ Fixture to create CoperniFUS viewer window. """
     window = Window(app=qapp)
+    window.switch_cached_settings_file('Test config', force_create_new=True)
     yield window
     window.close()
 
